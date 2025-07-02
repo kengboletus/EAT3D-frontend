2fd8e3a642c94925d5dc7a11951de07d34579c7e
Juno <98197980+fnuglord@users.noreply.github.com>
2025-07-02 12:01:12 +0800

    Configure tab icon active and inactive tin colors, add home icon from external library

--- app/(eshop)/_layout.tsx
+++ app/(eshop)/_layout.tsx
@@ -1,13 +1,26 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import Entypo from "react-native-vector-icons/Entypo";
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#8D8D8D",
+        tabBarInactiveTintColor: "#D9D9D9",
+      }}
+    >
       <Tabs.Screen
         name="home"
         options={{
           title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <Entypo
+              name="home"
+              color={focused ? "#8D8D8D" : "#D9D9D9"}
+              size={24}
+            />
+          ),
         }}
       />
       <Tabs.Screen
